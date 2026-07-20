1e2e36f7c1dd25f452b2b21663849e6c43032dc0
Mohd Ayatal <mohdayatal@gmail.com>
2026-07-21 04:26:30 +0530

    Time: 0 ms (100.00%) | Memory: 21.1 MB (85.65%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = {}
+        for x in nums:
+            if x not in count:
+                count[x] = 1
+            else:
+                count[x]+= 1
+        for x in count:
+            if count[x] > len(nums)//2:
+                return x       
